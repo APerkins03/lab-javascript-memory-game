a64cc5ef0af2da0cfc9b6440081572ab8461b429
Amber Moody <aperkins0922@gmail.com>
2023-05-09 11:45:53 -0400

    update

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+// console.log("memoryGame: ", memoryGame.cards[0]);
+
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -45,7 +47,41 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      console.log("Card clicked: ", card);
+
+      card.classList.toggle("turned");
+
+      memoryGame.pickedCards.push(card);
+
+      if(memoryGame.pickedCards.length === 2){
+        const card1 = memoryGame.pickedCards[0];
+        const card2 = memoryGame.pickedCards[1];
+
+        const card1Name = card1.getAttribute("data-card-name");
+        const card2Name = card2.getAttribute("data-card-name");
+
+        // console.log("card 1: ", card1Name, "<---> card 2: ", card2Name);
+
+        if(memoryGame.checkIfPair(card1Name, card2Name)){
+          card1.classList.add("blocked");
+          card2.classList.add("blocked");
+        } else {
+          setTimeout(() => {
+            card1.classList.toggle("turned");
+            card2.classList.toggle("turned");
+          }, 1000)
+        }
+
+        memoryGame.pickedCards = [];
+
+        document.getElementById("pairs-clicked").innerText = memoryGame.pairsClicked;
+        document.getElementById("pairs-guessed").innerText = memoryGame.pairsGuessed;
+
+        if(memoryGame.checkIfFinished()){
+          alert("You are the winner ⭐️")
+        }
+
+      }
     });
   });
 });
